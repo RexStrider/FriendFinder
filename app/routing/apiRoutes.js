976ebd09323b77b14ab1a9b465056444d28ed7c0
Odysseus <arwaterman@gmail.com>
2019-03-31 19:59:42 -0700

    updated test data

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,16 +1,10 @@
 const ROUTER = require(`express`).Router();
 const PATH = require(`path`);
+const DATA = require(`./../data/friends`);
 
-let data = [
-    {
-        "name":"Ahmed",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-        "scores":[ 5, 1, 4, 4, 5, 1, 2, 5, 4, 1 ]
-    }
-]
 
 const ATTACH_DATA = (req, res, next) => {
-    req.data = data;
+    req.data = DATA;
     next();
 }
 
