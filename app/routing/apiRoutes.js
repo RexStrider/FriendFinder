a505fe10d53875a91a1e4ca307536fb533701fdf
Odysseus <arwaterman@gmail.com>
2019-03-31 20:29:51 -0700

    updating api routes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,9 +8,22 @@ const ATTACH_DATA = (req, res, next) => {
     next();
 }
 
-ROUTER.get(`/`, ATTACH_DATA, (req, res) => {
+ROUTER.get(`/`, (req, res) => {
+    console.log(`Welcome to the API`);
+    res.send(`Welcome to the API`);
+});
+
+ROUTER.get(`/friends`, ATTACH_DATA, (req, res) => {
     console.log(req.data);
     res.send(req.data);
+});
+
+ROUTER.post(`/friends`, ATTACH_DATA, (req, res) => {
+    let person = req.body;
+
+    console.log(person);
+    DATA.push(person);
+    res.json(person);
 });
 
 module.exports = ROUTER;
