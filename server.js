cf02a3936d19c1c023f0f3ddf737d54f63d1207a
Odysseus <arwaterman@gmail.com>
2019-04-03 21:41:20 -0700

    updated path to css

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// const PATH = require(`path`);
+const PATH = require(`path`);
 
 const EXPRESS = require(`express`);
 
@@ -11,7 +11,7 @@ const APP = EXPRESS();
 APP.use(EXPRESS.urlencoded({ extended: true }));
 APP.use(EXPRESS.json());
 
-// APP.use(`/static`, express.static(PATH.join(__dirname, `app/static`)));
+APP.use(`/static`, EXPRESS.static(`app/static`));
 
 APP.use(ROUTES);
 
