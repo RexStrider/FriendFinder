81690cbc041980ac1a99785fae9e7f2a53e1cb85
Odysseus <arwaterman@gmail.com>
2019-08-25 16:07:53 -0700

    added image assets

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,17 +1,17 @@
 const DATA = [
     {
         "name":"Ahmed",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "photo":"/static/assets/images/ahmed.jpg",
         "scores":[ 5, 1, 4, 4, 5, 1, 2, 5, 4, 1 ]
     },
     {
         "name":"Cthulu",
-        "photo":"https://cdn-images-1.medium.com/max/1200/1*fmmF9LymBuB7uKXVYlshHQ.jpeg",
+        "photo":"/static/assets/images/cthulu.jpeg",
         "scores":[ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ]
     },
     {
         "name":"Mama Bear",
-        "photo":"https://static1.squarespace.com/static/54f911ace4b0f89f1295a513/557a3018e4b00537931798cf/595bf41472af65691b813357/1551918490390/bear+print.jpg?format=500w",
+        "photo":"/static/assets/images/mama-bear.jpg",
         "scores":[ 2, 3, 1, 5, 3, 3, 2, 5, 4, 1 ]
     }
 ]
